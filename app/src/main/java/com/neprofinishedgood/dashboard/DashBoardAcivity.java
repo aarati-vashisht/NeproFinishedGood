8a504f1a5baceee1026999f8fcb61a00220c5595
aarati_vashisht <aarati.vashisht2804@gmail.com>
2019-06-28 10:24:10 +0530

    Worked on Login with Static Data

---
+++ app/src/main/java/com/neprofinishedgood/dashboard/DashBoardAcivity.java
@@ -0,0 +1,18 @@
+package com.neprofinishedgood.dashboard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.neprofinishedgood.R;
+
+public class DashBoardAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board_acivity);
+
+
+    }
+}
---
+++ app/src/main/java/com/neprofinishedgood/login/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.neprofinishedgood.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.chaos.view.PinView;
+import com.neprofinishedgood.R;
+import com.neprofinishedgood.dashboard.DashBoardAcivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.buttonLogin)
+    Button buttonLogin;
+    @BindView(R.id.pinViewLogin)
+    PinView pinViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        getSupportActionBar().hide();
+        overridePendingTransition(0, 0);
+
+    }
+
+    @OnClick(R.id.buttonLogin)
+    public void onLoginButtonClick() {
+        if (pinViewLogin.getText().toString().equals("")) {
+            Toast.makeText(this, getString(R.string.plz_enter_pin), Toast.LENGTH_SHORT).show();
+        } else if (!pinViewLogin.getText().toString().equals("1234")) {
+            Toast.makeText(this, getString(R.string.invalid_pin), Toast.LENGTH_SHORT).show();
+        } else if (pinViewLogin.getText().toString().equals("1234")) {
+            Toast.makeText(this, getString(R.string.login_successfully), Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(this, DashBoardAcivity.class));
+            finish();
+            overridePendingTransition(0, 0);
+        }
+    }
+
+}
