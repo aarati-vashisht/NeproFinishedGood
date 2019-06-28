2531b329f923a8c132a3bb8e4045824c9929bd96
kartikpandey <dheeraj.pandey23995@gmail.com>
2019-06-28 16:51:26 +0530

    Changes made in Put Away Activity

--- app/src/main/java/com/neprofinishedgood/putaway/PutAwayActivity.java
+++ app/src/main/java/com/neprofinishedgood/putaway/PutAwayActivity.java
@@ -2,15 +2,38 @@ package com.neprofinishedgood.putaway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.neprofinishedgood.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class PutAwayActivity extends AppCompatActivity {
+
+    @BindView(R.id.frameEnterQuantity)
+    FrameLayout frameEnterQuantity;
+
+    @BindView(R.id.frameAssignFlt)
+    FrameLayout frameAssignFlt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_put_away);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.buttonConfirm)
+    public void onButtonConfirmClick(){
+        frameEnterQuantity.setVisibility(View.GONE);
+        frameAssignFlt.setVisibility(View.VISIBLE);
     }
 }
