c493e5fab655997d47940d2cf3683285cbad162e
Dheeraj Pandey <dheeraj.pandey23995@gmail.com>
2019-11-27 15:25:28 -0800

    Changes in Reject Stillage

--- app/src/main/java/com/neprofinishedgood/qualitycheck/model/RejectedInput.java
+++ app/src/main/java/com/neprofinishedgood/qualitycheck/model/RejectedInput.java
@@ -6,13 +6,82 @@ public class RejectedInput {
     String Shift;
     String Quantity;
     String Reason;
+    String ReasonName;
+    String IsKg;
+    String WorkOrderNo;
 
-    public RejectedInput(String stickerNo, String userId, String quantity, String reason, String shift) {
+    public RejectedInput(String stickerNo, String userId, String quantity, String reason, String reasonName, String shift, String isKg, String workOrderNo) {
         StickerNo = stickerNo;
         UserId = userId;
         Quantity = quantity;
         Reason = reason;
+        ReasonName = reasonName;
+        Shift = shift;
+        IsKg = isKg;
+        WorkOrderNo = workOrderNo;
+    }
+
+    public String getStickerNo() {
+        return StickerNo;
+    }
+
+    public void setStickerNo(String stickerNo) {
+        StickerNo = stickerNo;
+    }
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public String getShift() {
+        return Shift;
+    }
+
+    public void setShift(String shift) {
         Shift = shift;
     }
 
+    public String getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        Quantity = quantity;
+    }
+
+    public String getReason() {
+        return Reason;
+    }
+
+    public void setReason(String reason) {
+        Reason = reason;
+    }
+
+    public String getReasonName() {
+        return ReasonName;
+    }
+
+    public void setReasonName(String reasonName) {
+        ReasonName = reasonName;
+    }
+
+    public String getIsKg() {
+        return IsKg;
+    }
+
+    public void setIsKg(String isKg) {
+        IsKg = isKg;
+    }
+
+    public String getWorkOrderNo() {
+        return WorkOrderNo;
+    }
+
+    public void setWorkOrderNo(String workOrderNo) {
+        WorkOrderNo = workOrderNo;
+    }
 }
--- app/src/main/java/com/neprofinishedgood/qualitycheck/rejectquantity/adapter/RejectionListAdapter.java
+++ app/src/main/java/com/neprofinishedgood/qualitycheck/rejectquantity/adapter/RejectionListAdapter.java
@@ -1,24 +1,17 @@
 package com.neprofinishedgood.qualitycheck.rejectquantity.adapter;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.gson.Gson;
 import com.neprofinishedgood.R;
 import com.neprofinishedgood.move.model.ScanStillageResponse;
-import com.neprofinishedgood.pickandload.PickAndLoadAdapter;
-import com.neprofinishedgood.pickandload.PickAndLoadStillageActivity;
-import com.neprofinishedgood.pickandload.model.ScanLoadingPlanList;
-import com.neprofinishedgood.utils.Constants;
+import com.neprofinishedgood.qualitycheck.model.RejectedInput;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,55 +21,56 @@ import butterknife.ButterKnife;
 
 public class RejectionListAdapter extends RecyclerView.Adapter<RejectionListAdapter.ViewHolder> {
 
-    private final List<ScanStillageResponse> stillageDatumList;
-    private List<ScanStillageResponse> stillageDatumListFiltered;
+    private final ArrayList<RejectedInput> rejectionList;
+    private ArrayList<RejectedInput> rejectionListFiltered;
     private Context context;
     private View view;
 
-    public RejectionListAdapter(List<ScanStillageResponse> stillageDatumList) {
-        this.stillageDatumList = stillageDatumList;
-        this.stillageDatumListFiltered = stillageDatumList;
+    public RejectionListAdapter(ArrayList<RejectedInput> rejectionList) {
+        this.rejectionList = rejectionList;
+        this.rejectionListFiltered = rejectionList;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         context = parent.getContext();
-        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.stillage_layout, parent, false);
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rejection_list_layout, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.textViewitem.setText(stillageDatumListFiltered.get(position).getItemId());
-        holder.textViewQuantity.setText(stillageDatumListFiltered.get(position).getStandardQty() + "");
-        holder.textViewitemDesc.setText(stillageDatumListFiltered.get(position).getDescription());
-        holder.textViewStdQuantity.setText(stillageDatumListFiltered.get(position).getItemStdQty() + "");
-        holder.textViewNumber.setText(stillageDatumListFiltered.get(position).getStickerID());
+        holder.textViewStickerNo.setText(rejectionListFiltered.get(position).getStickerNo());
+        String unit = "";
+        if (rejectionListFiltered.get(position).getIsKg().equals("1")) {
+            unit = "Kg.";
+        } else {
+            unit = "Pcs.";
+        }
+        holder.textViewQuantity.setText(rejectionListFiltered.get(position).getQuantity() + " " + unit);
+        holder.textViewShift.setText(rejectionListFiltered.get(position).getShift());
+        holder.textViewReason.setText(rejectionListFiltered.get(position).getReasonName());
     }
 
     @Override
     public int getItemCount() {
-        return stillageDatumListFiltered.size();
+        return rejectionListFiltered.size();
     }
-
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final View mView;
 
-        @BindView(R.id.textViewitem)
-        public TextView textViewitem;
-
-        @BindView(R.id.textViewNumber)
-        public TextView textViewNumber;
+        @BindView(R.id.textViewStickerNo)
+        public TextView textViewStickerNo;
 
         @BindView(R.id.textViewQuantity)
         public TextView textViewQuantity;
 
-        @BindView(R.id.textViewStdQuatity)
-        public TextView textViewStdQuantity;
+        @BindView(R.id.textViewShift)
+        public TextView textViewShift;
 
-        @BindView(R.id.textViewitemDesc)
-        public TextView textViewitemDesc;
+        @BindView(R.id.textViewReason)
+        public TextView textViewReason;
 
         @BindView(R.id.cardView)
         CardView cardView;
