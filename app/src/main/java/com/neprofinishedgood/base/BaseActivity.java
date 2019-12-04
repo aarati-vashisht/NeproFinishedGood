a5fcfb73c94617b5e4a67328c7456631990e382e
Dheeraj Pandey <dheeraj.pandey23995@gmail.com>
2019-12-04 18:52:38 +0530

    worked on Different Activities

--- app/src/main/java/com/neprofinishedgood/base/BaseActivity.java
+++ app/src/main/java/com/neprofinishedgood/base/BaseActivity.java
@@ -61,7 +61,7 @@ public class BaseActivity extends AppCompatActivity implements IBaseInterface {
     public List<UniversalSpinner> siteList = new ArrayList<>();
 
     public int scanStillageLength = 8;
-    //    public int scanStillageLength = 13;
+//    public int scanStillageLength = 13;
     public int scanWorkOrderLength = 9;
     public int scanLocationLength = 11;
 
